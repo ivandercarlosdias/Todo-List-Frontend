d7bf6680ef26fc69ff6ec50c059d3d5d34fa1498
Ivander Carlos Dias <ivandercarlos@gmail.com>
2021-07-01 09:03:56 -0300

    Adicionado componente TodoList

---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,21 @@
+import './style.scss'
+
+export default function TodoList(): JSX.Element {
+    return (
+        <>
+            <h2>Task List</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                <li>Item 5</li>
+                <li>Item 6</li>
+                <li>Item 7</li>
+                <li>Item 8</li>
+                <li>Item 9</li>
+                <li>Item 10</li>
+            </ul>
+        </>
+    )
+}
